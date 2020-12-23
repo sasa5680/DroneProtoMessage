96f9dfb01556054d76caf2fc3373ea624bacad22
sasa5680 <sasa5680@naver.com>
2020-12-23 11:21:13 +0900

    pad

---
+++ src/main/java/Common/Pad.java
@@ -0,0 +1,18 @@
+package Common;
+
+public class Pad {
+	
+	public static byte None 	= 0;
+	
+	public static final byte F_Front  	= 1;
+	public static final byte F_Back 	= 2;
+	
+	public static final byte R_Left 	= 1;
+	public static final byte R_Right	= 2;
+	
+	public static final byte Y_C		= 1;
+	public static final byte Y_CC		= 2;
+	
+	public static final byte Th_up		= 1;
+	public static final byte Th_Down	= 2;
+}
