5fbe68320a8fd4ed0a0deeb7c84296ceb218ec28
sasa5680 <sasa5680@naver.com>
2021-01-27 00:06:32 +0900

    dd

--- src/main/java/Common/Pad.java
+++ src/main/java/Common/Pad.java
@@ -4,11 +4,11 @@ public class Pad {
 	
 	public static byte None 	= 0;
 	
-	public static final byte F_Front  	= 1;
-	public static final byte F_Back 	= 2;
+	public static final byte R_Front  	= 1;
+	public static final byte R_Back 	= 2;
 	
-	public static final byte R_Left 	= 1;
-	public static final byte R_Right	= 2;
+	public static final byte F_Left 	= 1;
+	public static final byte F_Right	= 2;
 	
 	public static final byte Y_C		= 1;
 	public static final byte Y_CC		= 2;
